00e30ddc46979aca3ae49f906992c22b0a95f91f
haessar <wahhill@gmail.com>
2018-12-13 16:10:50 +0000

    Add files via upload
    day 1

---
+++ 1.py
@@ -0,0 +1,33 @@
+# 1.1
+def iter_freq():
+	with open('1.txt') as f:
+		while True:
+			try:
+				yield int(f.readline())
+			except ValueError:
+				break
+
+# print sum(iter_freq())
+
+# 1.2
+def iter_cumsum(gen, init=0):
+    cumsum = init
+    for freq in gen:
+        cumsum += freq
+        yield cumsum
+
+
+def first_duplicate_freq():
+	counter = set()
+	idx = 1
+	init = 0
+	while True:	
+		for f in iter_cumsum(iter_freq(), init):
+			counter.add( f )
+			if len(counter) < idx:
+				print f
+				return
+			idx += 1
+		init = f
+
+first_duplicate_freq()
